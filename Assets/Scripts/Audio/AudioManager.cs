b93db09fbb2ab14ff8fe077f257be1aaab54d5e7
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2024-07-21 21:16:07 +0700

    add background music

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip background;
+    public AudioClip death;
+    public AudioClip attack;
+    public AudioClip shield;
+    public AudioClip pause;
+    public AudioClip click;
+    public AudioClip wallTouch;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
